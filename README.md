becbca61ddca377b3f246d13017dd97c4051d0b0
andy <andytwoods@gmail.com>
2018-11-28 11:53:26 +0000

    documentation

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
-# statistical_power
+# Pocket Statistics App
 
-A new Flutter project.
+An opensource proof-in-concept statistics app for Android and iPhone.
 
-## Getting Started
+Currently, the app consists of tools for calculating effect sizes based on Daniel Lakens 'Calculating Effect Sizes' [toolsheet](https://osf.io/vbdah/).
 
-For help getting started with Flutter, view our online
-[documentation](https://flutter.io/).
+It is my hope that we build from this and create something truly useful :) 
+
+Contributions most welcome. Please don't be daunted by the technology used! There are many ways to help, be it by porting statistical formula from other packages, improving documentation, or even spreading the word.
+
+## Technology behing the app
+The app is written in [Flutter](https://flutter.io), a wonderful new opensource package from Google for creating Android and IOS apps (and soon, hopefully, [desktop apps](https://github.com/google/flutter-desktop-embedding)). Flutter takes care of most of the hard stuff such as:
+* pretty elements on screen (text, images, inputs) 
+* screen positions (in rows, columns, lists etc).
+* transitions between pages
+* orientation   
+
+All coding in Flutter is done in [Dart](https://www.dartlang.org/) which is mostly straightforward. Flutter heavily relies on Object Orientated Programming with an app resembling a large tree structure with many many children and parent **widgets** (a key term -- most things are widgets in flutter) specifying both how the app will look and behave.  
+
+## How to Contribute
+
+###Installation
+
+Install Flutter by following the [official guide](https://flutter.io/docs/get-started/install). 
+
+You will need to decide on what IDE (code editor) you want to use. The guide assumes you will want to to install  Android Studio but you can also install Intellij IDEA Community (free) / Ultimate (paid for but [free for students/educators](https://www.jetbrains.com/student/)). I use Ultimate (and love it). 
+
+If you decided to use Intellij IDEA there are several plugins you need to install (follow this [guide](https://stackoverflow.com/questions/50485795/how-to-install-flutter-and-dart-in-android-studio-and-inttellij)). If you follow the official guide's next step by [making your first app](https://flutter.io/docs/get-started/test-drive), these will be setup for you.
+ 
+ You clone this repo within the Intellij via VCS -> Checkout From Version Control (select git and paste in this url https://github.com/andytwoods/statistical_power_app).
+
+I develop using a virtual Android device. You can set one up in Intellij via Tools -> Android -> AVD Manager (if that option is disabled, here is a [solution](https://stackoverflow.com/questions/53497851/avd-manager-in-intellij-is-disabled/53497862#53497862)). I created a virtual Pixel 2. It is very easy though to also develop with actual androids or ios devices (in most cases, you just need to put your device in 'developer mode'). You cannot develop on a PC with an IOS device, however (although perhaps this could be done using Virtual Machines via e.g. VirtualBox).
+
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -30,7 +30,6 @@ class StatsDrawer extends StatefulWidget {
 }
 
 class StatsDrawerState extends State<StatsDrawer> {
-
   TextEditingController searchController = TextEditingController();
 
   @override
@@ -38,80 +37,75 @@ class StatsDrawerState extends State<StatsDrawer> {
     List<MapEntry<String, Function>> pagesList = pages.entries.toList();
 
     String searchFor = searchController.text.toLowerCase();
-    if(searchFor.length>0){
-      pagesList = pagesList.where((MapEntry<String, Function> pe){
+    if (searchFor.length > 0) {
+      pagesList = pagesList.where((MapEntry<String, Function> pe) {
         return pe.key.toLowerCase().contains(searchFor);
       }).toList();
     }
 
-    return new Drawer(
-        child: new ListView.builder(
-
-            itemBuilder: (BuildContext context, int index) {
-              if (index == 0)
-                return DrawerHeader(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return new Drawer(child:
+        new ListView.builder(itemBuilder: (BuildContext context, int index) {
+      if (index == 0)
+        return DrawerHeader(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Row(
                     children: <Widget>[
-                      Align(
-                          alignment: Alignment.centerLeft,
-                          child: Row(
-                            children: <Widget>[
-                              Text("POCKET STATISTICS"),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 8.0),
-                                child: InkWell(
-                                  onTap: (){
-                                    showAboutDialog(
-                                      context: context,
-                                        applicationLegalese: 'github.com/andytwoods/statistical_power_app\n\nGNU General Public License v3.0'
-
-                                    );
-
-
-                                  },
-                                  child: Icon(Icons.info, color: Colors.black45,),
-                                ),
-                              )
-                            ],
-                          )),
-                      Container(
-                        decoration: new BoxDecoration(
-                            borderRadius: new BorderRadius.all(
-                                  Radius.circular(10.0))
-                        ),
-                        child: TextField(
-                          controller: searchController,
-                          onChanged: (_)=> setState(() {}), //pokes for a regeneration
-                          decoration: InputDecoration(
-                            contentPadding: const EdgeInsets.all(1.0),
-                            border: InputBorder.none,
-                            icon: Icon(Icons.search),
-                            labelText: 'search...',
+                      Text("POCKET STATISTICS"),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: InkWell(
+                          onTap: () {
+                            showAboutDialog(
+                                context: context,
+                                applicationLegalese:
+                                    'github.com/andytwoods/statistical_power_app\n\nGNU General Public License v3.0');
+                          },
+                          child: Icon(
+                            Icons.info,
+                            color: Colors.black45,
                           ),
-
                         ),
                       )
                     ],
+                  )),
+              Container(
+                decoration: new BoxDecoration(
+                    borderRadius: new BorderRadius.all(Radius.circular(10.0))),
+                child: TextField(
+                  controller: searchController,
+                  onChanged: (_) => setState(() {}),
+                  //pokes for a regeneration
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.all(1.0),
+                    border: InputBorder.none,
+                    icon: Icon(Icons.search),
+                    labelText: 'search...',
                   ),
-                  decoration: new BoxDecoration(color: Colors.lightGreenAccent),
-                );
-              index -= 1;
+                ),
+              )
+            ],
+          ),
+          decoration: new BoxDecoration(color: Colors.lightGreenAccent),
+        );
+      index -= 1;
 
+      if (index < pagesList.length) {
+        MapEntry<String, Function> m = pagesList[index];
+        return new ListTile(
+          title: new Text(m.key),
+          onTap: () {
+            widget.selectedCallback(m.value());
+            Navigator.of(context).pop();
+          },
+        );
+      }
 
-              if(index< pagesList.length) {
-                MapEntry<String, Function> m = pagesList[index];
-                return new ListTile(
-                  title: new Text(m.key),
-                  onTap: () {
-                    widget.selectedCallback(m.value());
-                    Navigator.of(context).pop();
-                  },
-                );
-              }
-
-              return null;
-            }));
+      return null;
+    }));
   }
 }
